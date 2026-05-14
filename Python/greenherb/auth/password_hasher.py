ca4433067d8c9d5a3e9b584c1670143ce66b74e0
Guilherme Soeiro <163298631+GSoeiro@users.noreply.github.com>
2026-05-14 17:12:12 +0100

    Sprint 3

--- Python/greenherb/auth/password_hasher.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-
-class PasswordHasher:
-    def hash(self, password: str) -> str:
-        return hashlib.sha256(password.encode("utf-8")).hexdigest()
-
-    def verify(self, password: str, password_hash: str) -> bool:
-        return self.hash(password) == password_hash
